6fb5015488b3c0e611f639cdd831f8f6ce35869f
Chhodia <97937732+Chhodia@users.noreply.github.com>
2023-10-30 10:28:43 +0200

    Add files via upload
    i commited

--- 7uzd.py
+++ 7uzd.py
@@ -6,3 +6,12 @@ pievienots klāt doktora nosaukums - Dr.
 
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
 sarakts2=[]
+for i in saraksts1:
+    x = 'Dr. ' + str(i)
+    sarakts2.append(x)
+print(sarakts2)
+
+def add_dr(i):
+    return 'Dr. ' + str(i)
+sarakts2 = list(map(add_dr, saraksts1))
+print(sarakts2)
